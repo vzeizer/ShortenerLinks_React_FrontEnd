1091280ccc80dc69dca1c620426edfbf3d087cf2
vzeizer <vzeizer@gmail.com>
2025-10-26 16:03:37 -0300

    feat: update dependencies and enhance Tailwind CSS configuration
    - Added new dependencies: lucide-react, tailwind-merge, and tailwind-variants.
    - Extended Tailwind CSS configuration with custom font families, sizes, weights, and a corrected color palette.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,41 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+      },
+      fontSize: {
+        'xs': ['10px', '14px'],
+        'sm': ['12px', '16px'],
+        'md': ['14px', '18px'],
+        'lg': ['18px', '24px'],
+        'xl': ['24px', '32px'],
+      },
+      fontWeight: {
+        regular: '400',
+        semibold: '600',
+        bold: '700',
+      },
+      // --- PALETA DE CORES CORRIGIDA ---
+      colors: {
+        'brand': {
+          'base': '#2C46B1', // blue-base
+          'dark': '#2C4091', // blue-dark
+        },
+        'danger': '#B12C4D', // danger
+        'gray': {
+          100: '#F9F9FB', // gray-100
+          200: '#E4E6EC', // gray-200
+          300: '#CDCFD5', // gray-300
+          400: '#74798B', // gray-400
+          500: '#4D505C', // gray-500
+          600: '#1F2025', // gray-600
+        },
+        'white': '#FFFFFF', // white
+      }
+      // ---------------------------------
+    },
   },
   plugins: [],
 }
